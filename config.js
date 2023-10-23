8926e79af0a219e0511297befab6675ab08f0a3f
Parth Savani <parthsavani@Parths-MacBook-Air.local>
2023-10-23 17:35:45 +0530

    First

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ dotenv.config();
 export default {
     PORT: process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://intrexitx:12345@cluster0.nac9c5q.mongodb.net/?retryWrites=true&w=majority',
-    JWT_SECRET: process.env.JWT_SECRET || 'somethingssecret',
-    R_KeyId:process.env.R_KeyId || 'rzp_test_c5z6eRGaOgyPLd',
-    RAZOR_PAY_KEY_SECRET:process.env.RAZOR_PAY_KEY_SECRET || 'vRzzCxaeJ2ktBLOJobk0cqpN'
+   
     };
   
 
