df4376e2952785ef799ef9b55bc1eb1433309928
parthsavani007 <147747524+parthsavani007@users.noreply.github.com>
2023-10-24 17:30:36 +0530

    Update productRouter.js

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -2,7 +2,6 @@ import express from 'express';
 import Product from '../models/productModel';
 
 
-import { isAdmin, isAuth, isSellerOrAdmin } from '../util.js';
 import data from '../data';
 
 const router = express.Router();
@@ -11,61 +10,12 @@ const router = express.Router();
 
 router.get('/get', async (req, res) => {
   
-  const pageSize = 6;
-  const page = Number(req.query.pageNumber) || 1;
   const products = await Product.find();
   
   res.send( products );
 }
 );
-// router.get('/', async (req, res) => {
-  
-//   const pageSize = 6;
-//   const page = Number(req.query.pageNumber) || 1;
-//   const searchKeyword = req.query.searchKeyword || '';;
- 
-//   const category = req.query.category || '';
-//   const order = req.query.order || '';
-//   const min =
-//     req.query.min && Number(req.query.min) !== 0 ? Number(req.query.min) : 0;
-//   const max =
-//     req.query.max && Number(req.query.max) !== 0 ? Number(req.query.max) : 0;
-//   const rating =
-//     req.query.rating && Number(req.query.rating) !== 0
-//       ? Number(req.query.rating)
-//       : 0;
 
-//   const priceFilter = min && max ? { price: { $gte: min, $lte: max } } : {};
-//   const ratingFilter = rating ? { rating: { $gte: rating } } : {};
-//   const sortOrder = order === 'lowest' ? { price: 1 }: order === 'highest' ? { price: -1 }: order === 'toprated'? { rating: -1 } : { _id: -1 };
-//   const nameFilter = searchKeyword ? { name: { $regex: searchKeyword, $options: 'i' } } : {};
-//   const seller = req.query.seller || '';
-//   const sellerFilter = seller ? { seller } : {};
-//   const categoryFilter = category ? { category } : {};
-//   const count = await Product.countDocuments({
-//     ...sellerFilter,
-//     ...nameFilter,
-//     ...categoryFilter,
-//     ...priceFilter,
-//     ...ratingFilter,
-//   });
-//   const products = await Product.find({...sellerFilter,
-//     ...nameFilter,...categoryFilter,...priceFilter,
-//     ...ratingFilter,
-//   }).populate('seller', 'seller.name seller.logo').sort(sortOrder).skip(pageSize * (page - 1))
-//   .limit(pageSize);
-//   //const products = await Product.find(nameFilter).sort(order);
-
-//   res.send({ products, page, pages: Math.ceil(count / pageSize) });
-// });
-
-// router.get('/categories',async (req, res) => {
-   
-    
-  //     const categories = await Product.find().distinct('category');
-  //     res.send(categories);
-//   }
-// );
 router.get('/:id', async (req, res) => {
   console.log(req.params.id)
   const productId = req.params.id;
@@ -84,87 +34,7 @@ router.get('/:id', async (req, res) => {
  
  
 });
- router.get('/det/:code', async (req, res) => {
-   console.log(req.params.id)
-    //  const nameFilter = req.params.id ? { name: { $regex: req.params.id.split("")[0], $options: 'i' } } : {};
-
-   try {
-    const product = await Product.find({ code: { $regex:req.params.code.split("-").join(""), $options: 'i' } }  )
-    console.log(product.length);
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: 'Product Not Found.' });
-    }
-    
-   } catch (error) {
-    res.status(404).send({ message: error });
-   }
-  
-  
-});
-router.put('/:id',isAuth, async (req, res) => {
-  const productId = req.params.id;
-  const product = await Product.findById(productId);
-  
-  if (product) {
-     
-     product.name = req.body.name;
-     product.price = req.body.price;
-     product.priceSale = req.body.priceSale;
-     product.sku = req.body.sku;
-     product.cover = req.body.images[0];
-     product.images = req.body.images;
-     product.code = req.body.code;
-     product.tag = req.body.tag;
-     product.status = req.body.status;
-     product.inventoryType= req.body.inventoryType;     
-     product.description = req.body.description;
-     product.gender = req.body.gender;
-     product.category = req.body.category;
-     product.colors = req.body.colors;
-     product.available = req.body.available;
-     const updatedProduct = await product.save(); 
-    if (product) {
-      return res
-        .status(200)
-        .send({ message: 'Product Updated', data: product });
-    }
-  }
-  return res.status(500).send({ message: ' Error in Updating Product.' });
-});
-
-router.delete('/:id',isAuth,async (req, res) => {
-  const deletedProduct = await Product.findById(req.params.id);
-  if (deletedProduct) {
-    await deletedProduct.remove();
-    res.send({ message: 'Product Deleted' });
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
-// // router.post('/',isAuth, isAdmin, async (req, res) => {
-  
-// //   const product = new Product({
-// //     name: req.body.name,
-// //     price: req.body.price,
-// //     image: req.body.image,
-// //     brand: req.body.brand,
-// //     category: req.body.category,
-// //     countInStock: req.body.countInStock,
-// //     description: req.body.description,
-// //     rating: req.body.rating,
-// //     numReviews: req.body.numReviews,
-// //   });
-// //   const newProduct = await product.save();
-// //   if (newProduct) {
-// //     return res
-// //       .status(201)
-// //       .send({ message: 'New Product Created', data: newProduct });
-// //   }
-// //   return res.status(500).send({ message: ' Error in Creating Product.' });
-// // });
+ 
 
 router.post(
   '/',async (req, res) => {
@@ -181,35 +51,6 @@ router.post(
   }
 );
 
-// router.post('/:id/reviews', isAuth,async (req, res) => {
-//     const productId = req.params.id;
-//     const product = await Product.findById(productId);
-//     if (product) {
-//       if (product.reviews.find((x) => x.name === req.user.name)) {
-//         return res
-//           .status(400)
-//           .send({ message: 'You already submitted a review' });
-//       }
-//       const review = {
-//         name: req.user.name,
-//         rating: Number(req.body.rating),
-//         comment: req.body.comment,
-//       };
-//       product.reviews.push(review);
-//       product.numReviews = product.reviews.length;
-//       product.rating =
-//         product.reviews.reduce((a, c) => c.rating + a, 0) /
-//         product.reviews.length;
-//       const updatedProduct = await product.save();
-//       res.status(201).send({
-//         message: 'Review Created',
-//         review: updatedProduct.reviews[updatedProduct.reviews.length - 1],
-//       });
-//     } else {
-//       res.status(404).send({ message: 'Product Not Found' });
-//     }
-//   }
-// );
 
 
 export default router;
